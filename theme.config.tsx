53d713d8c73ed43279ea46cf72507fde2cee9161
2dayevents <simone.priuli@icloud.com>
2022-12-21 19:03:44 +0100

    add image to navbar

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>IMongo</span>,
+  logo: <span><img src="https://raw.githubusercontent.com/simonepriuli/imongo/622fcca6ef66de9140fa4287147ae08b74b7d46a/static/icon.svg" alt="IMongo logo" height={28} width={28}/></span>,
   project: {
     link: 'https://github.com/simonepriuli/imongo',
   },
